414778c0c110c04c279d42e5fcc2642334b25f0e
edward <edwardf1@uci.edu>
2025-04-10 18:04:18 -0700

    testing main

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -16,6 +16,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+	//testing
 	public static void main(String[] args) {
 		// only arg that matches following will work
 		Set<String> validArgNames = Set.of("covid", "properties", "population", "log");
